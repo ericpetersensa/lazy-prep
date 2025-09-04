295c3505b79c81dc3fd9611251aeb5f13a540d4e
Eric Petersen <ericpetersensa@gmail.com>
2025-09-04 08:43:04 -0700

    Update secrets.js

--- scripts/parts/secrets.js
+++ scripts/parts/secrets.js
@@ -1,17 +1,29 @@
+/* Secrets & Clues tab logic (v13-ready) */
 export function activateSecretsListeners(html, app) {
-  html.find(".add-secret").click(() => {
-    app.session.secrets.push({ id: randomID(), text: "", revealed: false });
+  // Add secret
+  html.on("click", ".add-secret", () => {
+    app.session.secrets.push({
+      id: foundry.utils.randomID(),
+      text: "",
+      revealed: false
+    });
     app.render();
   });
 
-  html.find("input[type='text'][name^='secret-']").change(ev => {
+  // Edit secret text
+  html.on("change", "input[type='text'][name^='secret-']", (ev) => {
     const id = ev.currentTarget.name.split("-")[1];
-    const secret = app.session.secrets.find(s => s.id === id);
-    if (secret) secret.text = ev.currentTarget.value;
+    const s = app.session.secrets.find(x => x.id === id);
+    if (s) s.text = ev.currentTarget.value ?? "";
   });
 
-  html.find("input[type='checkbox']").change(ev => {
-    const idx = html.find("input[type='checkbox']").index(ev.currentTarget);
-    app.session.secrets[idx].revealed = ev.currentTarget.checked;
+  // Toggle revealed
+  // (Template's checkbox has no name; we infer the ID from the sibling text input in the same <li>)
+  html.on("change", ".tab[data-tab='secrets'] li input[type='checkbox']", (ev) => {
+    const $li = $(ev.currentTarget).closest("li");
+    const name = $li.find("input[name^='secret-']").attr("name"); // "secret-<id>"
+    const id = name?.split("-")[1];
+    const s = app.session.secrets.find(x => x.id === id);
+    if (s) s.revealed = ev.currentTarget.checked;
   });
 }
