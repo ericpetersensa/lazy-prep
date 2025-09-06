66778ad73f3c91f5fd498279d9f92436f89bd84d
Eric Petersen <ericpetersensa@gmail.com>
2025-09-06 11:49:16 -0700

    Update strongStart.js

--- scripts/parts/strongStart.js
+++ scripts/parts/strongStart.js
@@ -5,7 +5,6 @@ export function activateStrongStartListeners(htmlElement, app) {
       app.session.strongStart.text = ev.currentTarget.value ?? "";
     });
   });
-
   htmlElement.querySelectorAll("input[name='strongStart-sceneId']").forEach(input => {
     input.addEventListener("change", ev => {
       app.session.strongStart.sceneId = ev.currentTarget.value ?? null;
